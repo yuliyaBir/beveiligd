70a0c125334c50b61438013621b65704fee642dd
yuliyaBir <zhivo4ka@gmail.com>
2023-12-05 14:45:46 +0100

    Initial commit

---
+++ src/main/java/be/vdab/beveiligd/BeveiligdApplication.java
@@ -0,0 +1,13 @@
+package be.vdab.beveiligd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeveiligdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeveiligdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/vdab/beveiligd/BeveiligdApplicationTests.java
@@ -0,0 +1,13 @@
+package be.vdab.beveiligd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeveiligdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
